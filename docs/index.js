276830f1d66471163623a90019216a2145a50c6a
jvlerner <jvlerner@uolinc.com>
2024-07-10 02:08:32 -0300

    fix: prettier css and js

--- docs/index.js
+++ docs/index.js
@@ -1,88 +1,86 @@
-
 const regex = /[^a-z0-9\s{0,}]/gm;
 const text_input = document.getElementById("text_input");
-const text_output = document.getElementById("text_output")
-const handler_output = document.getElementById("container_handler_output")
-const result_output = document.getElementById("container_result_output")
+const text_output = document.getElementById("text_output");
+const handler_output = document.getElementById("container_handler_output");
+const result_output = document.getElementById("container_result_output");
 
 function encodeText() {
-    // get text of textarea
-    let text = text_input.value;
-    text = text.toString();
-    // check string is lowercase and dont has special caracters
-    let validateText = regex.test(text);
-    let textLowerCase = text.toLowerCase()
-    let validadeLowerCase = textLowerCase == text ? true : false;
-    // set default handler for container
-    handler_output.style.display = "flex";
-    result_output.style.display = "none";
+  // get text of textarea
+  let text = text_input.value;
+  text = text.toString();
+  // check string is lowercase and dont has special caracters
+  let validateText = regex.test(text);
+  let textLowerCase = text.toLowerCase();
+  let validadeLowerCase = textLowerCase == text ? true : false;
+  // set default handler for container
+  handler_output.style.display = "flex";
+  result_output.style.display = "none";
 
-    console.log("Text "+text);
-    console.log("Regex "+validateText);
+  console.log("Text " + text);
+  console.log("Regex " + validateText);
 
-    if (text == "" || text == "undefined" || text == null || text == "null") {
-        alert("Você deve digitar um texto para criptografar!");
-        text_input.focus();
-        return
-    }
+  if (text == "" || text == "undefined" || text == null || text == "null") {
+    alert("Você deve digitar um texto para criptografar!");
+    text_input.focus();
+    return;
+  }
 
-    if (validateText == true || validadeLowerCase == false ) {
-        alert("O texto deve conter apenas letras minúsculas e sem acento!");
-        text_input.focus();
-        return
-    }
+  if (validateText == true || validadeLowerCase == false) {
+    alert("O texto deve conter apenas letras minúsculas e sem acento!");
+    text_input.focus();
+    return;
+  }
 
-    if (validateText == false) {
-        text = text.replaceAll("e", "enter");
-        text = text.replaceAll("i", "imes");
-        text = text.replaceAll("a", "ai");
-        text = text.replaceAll("o", "ober");
-        text = text.replaceAll("u", "ufat");
-        text_output.innerHTML = text;
-        handler_output.style.display = "none";
-        result_output.style.display = "flex";
-        result_output.style.flexDirection = "column";
-        text_input.value = "";
-        return
-    }
- 
+  if (validateText == false) {
+    text = text.replaceAll("e", "enter");
+    text = text.replaceAll("i", "imes");
+    text = text.replaceAll("a", "ai");
+    text = text.replaceAll("o", "ober");
+    text = text.replaceAll("u", "ufat");
+    text_output.innerHTML = text;
+    handler_output.style.display = "none";
+    result_output.style.display = "flex";
+    result_output.style.flexDirection = "column";
+    text_input.value = "";
+    return;
+  }
 }
 
 function decodeText() {
-    // get text of textarea
-    let text = text_input.value;
-    text = text.toString();
-    // check string is lowercase and dont has special caracters
-    let validateText = regex.test(text);
-    // set default handler for container
-    handler_output.style.display = "flex";
-    result_output.style.display = "none";
+  // get text of textarea
+  let text = text_input.value;
+  text = text.toString();
+  // check string is lowercase and dont has special caracters
+  let validateText = regex.test(text);
+  // set default handler for container
+  handler_output.style.display = "flex";
+  result_output.style.display = "none";
 
-    if ( text == "" || text == "undefined" || text == null || text == "null" ) {
-        alert("Você deve digitar um texto para descriptografar!");
-        text_input.focus();
-        return
-    }
+  if (text == "" || text == "undefined" || text == null || text == "null") {
+    alert("Você deve digitar um texto para descriptografar!");
+    text_input.focus();
+    return;
+  }
 
-    if ( validateText == true || validadeLowerCase == false ) {
-        alert("O texto deve conter apenas letras minúsculas e sem acento!");
-        text_input.focus();
-        return
-    }
+  if (validateText == true || validadeLowerCase == false) {
+    alert("O texto deve conter apenas letras minúsculas e sem acento!");
+    text_input.focus();
+    return;
+  }
 
-    if ( validateText == false ) {
-        text = text.replaceAll("enter", "e");
-        text = text.replaceAll("imes", "i");
-        text = text.replaceAll("ai", "a");
-        text = text.replaceAll("ober", "o");
-        text = text.replaceAll("ufat", "u");
-        text_output.innerHTML = text;
-        handler_output.style.display = "none";
-        result_output.style.display = "flex";
-        result_output.style.flexDirection = "column";
-        text_input.value = "";
-        return
-    }
+  if (validateText == false) {
+    text = text.replaceAll("enter", "e");
+    text = text.replaceAll("imes", "i");
+    text = text.replaceAll("ai", "a");
+    text = text.replaceAll("ober", "o");
+    text = text.replaceAll("ufat", "u");
+    text_output.innerHTML = text;
+    handler_output.style.display = "none";
+    result_output.style.display = "flex";
+    result_output.style.flexDirection = "column";
+    text_input.value = "";
+    return;
+  }
 }
 
 function copyText() {
